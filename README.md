67541fd395d8b7bd14fbcd951d983bd4b132d406
David Miles <davidmiles1925@gmail.com>
2024-03-15 13:50:52 -0500

    refactor and update readme

--- README.md
+++ README.md
@@ -1,23 +1,39 @@
 # Office Stoplight
 
-## Inspiration
+## Project Decription
 
-I work from home, and my workspace also happens to directly attatch to the garage. Because of this, my family often needs access to the garage, but does not know if I can be disturbed. This system will give them a definitive answer, by placing adjustable LEDs on the outside of the office and garage doors. 
+I work from home, and my workspace also happens to directly attatch to the garage. Because of this, my family often needs access to the garage, but does not know if I can be disturbed. This system will give them a definitive answer, by placing adjustable LEDs on the outside of the office and garage doors.
 
-## Use
+Please feel free to use everything here to build your own.
+
+## Repo Contents
+
+1. Project Diagram - This is meant to aid in the construction of the circuitry. Please feel free to reach out if you have questions.
+
+2. Code - The "stoplight.py" file listed contains the code to run the program.
+
+3. 3D Models - These contain print casings and mountings for the device.
+
+4. Link to set up your Raspberry Pi zero, and get it running the program on startup: **[Click Here](https://github.com/DavidMiles1925/pi_zero_setup)**
+
+## Diagram
+
+## What the Lights Mean
 
 - **RED:** DO NOT DISTURB
 - **YELLOW:** You can come in, but be quiet.
 - **GREEN:** Come on in!
 
-## Progress:
+## Description and Gallery
 
-- Code is written
-- Device circuitry is tested
+**Prototying**
 
-[PHOTO HERE]
+![Prototype](./media/Prototyping.png)
 
-## To Do:
+**Main Unit - Final**
 
-- Set up Pi Zero
-- Build full working version
+![Main Unit](./media/main_unit.jpg)
+
+**Door Unit - Final**
+
+![Door Unit](./media/external_unit.jpg)
--- stoplight.py
+++ stoplight.py
@@ -13,6 +13,9 @@ RED_BUTTON_PIN = 23
 YELLOW_BUTTON_PIN = 24
 GREEN_BUTTON_PIN = 25
 
+LED_PIN_ARRAY = [INDICATOR_LED_PIN, RED_BUTTON_PIN, YELLOW_BUTTON_PIN, GREEN1_LED_PIN, GREEN2_LED_PIN]
+BUTTON_PIN_ARRAY = [BLUE_BUTTON_PIN, RED_BUTTON_PIN, YELLOW_BUTTON_PIN, GREEN_BUTTON_PIN]
+
 def setup_pins():
     # Board Mode: BCM
     GPIO.setmode(GPIO.BCM)
@@ -20,46 +23,56 @@ def setup_pins():
     # Disable Warnings
     GPIO.setwarnings(False)
 
+    for x in range(len(LED_PIN_ARRAY)):
+        GPIO.setup(LED_PIN_ARRAY[x], GPIO.OUT)
+        GPIO.output(LED_PIN_ARRAY[x], GPIO.LOW)
     # LEDs
-    GPIO.setup(INDICATOR_LED_PIN, GPIO.OUT)
-    GPIO.output(INDICATOR_LED_PIN, GPIO.LOW)
+    # GPIO.setup(INDICATOR_LED_PIN, GPIO.OUT)
+    # GPIO.output(INDICATOR_LED_PIN, GPIO.LOW)
 
-    GPIO.setup(RED_LED_PIN, GPIO.OUT)
-    GPIO.output(RED_LED_PIN, GPIO.LOW)
+    # GPIO.setup(RED_LED_PIN, GPIO.OUT)
+    # GPIO.output(RED_LED_PIN, GPIO.LOW)
 
-    GPIO.setup(YELLOW_LED_PIN, GPIO.OUT)
-    GPIO.output(YELLOW_LED_PIN, GPIO.LOW)
+    # GPIO.setup(YELLOW_LED_PIN, GPIO.OUT)
+    # GPIO.output(YELLOW_LED_PIN, GPIO.LOW)
 
-    GPIO.setup(GREEN1_LED_PIN, GPIO.OUT)
-    GPIO.output(GREEN1_LED_PIN, GPIO.LOW)
+    # GPIO.setup(GREEN1_LED_PIN, GPIO.OUT)
+    # GPIO.output(GREEN1_LED_PIN, GPIO.LOW)
 
-    GPIO.setup(GREEN2_LED_PIN, GPIO.OUT)
-    GPIO.output(GREEN2_LED_PIN, GPIO.LOW)
+    # GPIO.setup(GREEN2_LED_PIN, GPIO.OUT)
+    # GPIO.output(GREEN2_LED_PIN, GPIO.LOW)
 
+    for x in range(len(BUTTON_PIN_ARRAY)):
+        GPIO.setup(BUTTON_PIN_ARRAY[x], GPIO.OUT)
     # Buttons
-    GPIO.setup(BLUE_BUTTON_PIN, GPIO.IN)
-    GPIO.setup(RED_BUTTON_PIN, GPIO.IN)
-    GPIO.setup(YELLOW_BUTTON_PIN, GPIO.IN)
-    GPIO.setup(GREEN_BUTTON_PIN, GPIO.IN)
+    # GPIO.setup(BLUE_BUTTON_PIN, GPIO.IN)
+    # GPIO.setup(RED_BUTTON_PIN, GPIO.IN)
+    # GPIO.setup(YELLOW_BUTTON_PIN, GPIO.IN)
+    # GPIO.setup(GREEN_BUTTON_PIN, GPIO.IN)
 
 
 def all_leds(status):
 
     if status == "on" or status == "ON":
-        GPIO.output(RED_LED_PIN, GPIO.HIGH)
-        GPIO.output(YELLOW_LED_PIN, GPIO.HIGH)
-        GPIO.output(GREEN1_LED_PIN, GPIO.HIGH)
-        GPIO.output(GREEN2_LED_PIN, GPIO.HIGH)
+        for x in range(len(LED_PIN_ARRAY)):
+            GPIO.output(LED_PIN_ARRAY[x], GPIO.HIGH)
+        # GPIO.output(RED_LED_PIN, GPIO.HIGH)
+        # GPIO.output(YELLOW_LED_PIN, GPIO.HIGH)
+        # GPIO.output(GREEN1_LED_PIN, GPIO.HIGH)
+        # GPIO.output(GREEN2_LED_PIN, GPIO.HIGH)
 
     else:
-        GPIO.output(RED_LED_PIN, GPIO.LOW)
-        GPIO.output(YELLOW_LED_PIN, GPIO.LOW)
-        GPIO.output(GREEN1_LED_PIN, GPIO.LOW)
-        GPIO.output(GREEN2_LED_PIN, GPIO.LOW)
+        for x in range(len(LED_PIN_ARRAY)):
+            GPIO.output(LED_PIN_ARRAY[x], GPIO.LOW)
+        # GPIO.output(RED_LED_PIN, GPIO.LOW)
+        # GPIO.output(YELLOW_LED_PIN, GPIO.LOW)
+        # GPIO.output(GREEN1_LED_PIN, GPIO.LOW)
+        # GPIO.output(GREEN2_LED_PIN, GPIO.LOW)
 
 def led_on(pin):
     all_leds("off")
 
+    GPIO.output(INDICATOR_LED_PIN, GPIO.HIGH)
     GPIO.output(pin, GPIO.HIGH)
 
 def green_on():
@@ -98,6 +111,37 @@ def light_show():
     led_on(RED_LED_PIN)
     
 
+def check_buttons():
+    if GPIO.input(RED_BUTTON_PIN) == False and GPIO.input(YELLOW_BUTTON_PIN) == False:
+        light_show()
+
+
+    if GPIO.input(BLUE_BUTTON_PIN) == False and GPIO.input(RED_BUTTON_PIN) == False:
+        print("DONE")
+        GPIO.cleanup()
+        exit()
+
+        
+    if GPIO.input(BLUE_BUTTON_PIN) == False:
+        all_leds("off")
+        print("OFF")
+        sleep(0.2)
+
+    elif GPIO.input(RED_BUTTON_PIN) == False:
+        print("RED")
+        led_on(RED_LED_PIN)
+        sleep(0.2)
+
+    elif GPIO.input(YELLOW_BUTTON_PIN) == False:
+        print("YELLOW")
+        led_on(YELLOW_LED_PIN)
+        sleep(0.2)
+
+    elif GPIO.input(GREEN_BUTTON_PIN) == False:
+        print(f"GREEN")
+        green_on()
+        sleep(0.2)
+
 
 if __name__ == "__main__":
     setup_pins()
@@ -108,37 +152,7 @@ if __name__ == "__main__":
 
     try:
         while True:
-
-            if GPIO.input(GREEN_BUTTON_PIN) == False and GPIO.input(YELLOW_BUTTON_PIN) == False:
-                light_show()
-
-
-            if GPIO.input(BLUE_BUTTON_PIN) == False and GPIO.input(RED_BUTTON_PIN) == False:
-                print("DONE")
-                GPIO.cleanup()
-                exit()
-
-                
-            if GPIO.input(BLUE_BUTTON_PIN) == False:
-                all_leds("off")
-                print("OFF")
-                sleep(0.2)
-
-            elif GPIO.input(RED_BUTTON_PIN) == False:
-                print("RED")
-                led_on(RED_LED_PIN)
-                sleep(0.2)
-
-            elif GPIO.input(YELLOW_BUTTON_PIN) == False:
-                print("YELLOW")
-                led_on(YELLOW_LED_PIN)
-                sleep(0.2)
-
-            elif GPIO.input(GREEN_BUTTON_PIN) == False:
-                print(f"GREEN")
-                green_on()
-                sleep(0.2)
-
+            check_buttons()
 
     except KeyboardInterrupt:
         print("DONE")
